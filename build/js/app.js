4eca8feb3a13a55aee4d4cf0f3f1aff772d29ce2
Michael Lesher <mike@thehivemedia.com>
2013-01-09 17:11:48 -0500

    Initial commit of codebase

---
+++ build/js/app.js
@@ -0,0 +1,13 @@
+// If using JSLint for syntax debugging, include the following two lines for better management
+/*jslint browser: true, sloppy: true, white: true*/
+/*global $, console, alert, app*/
+
+$(function() { 
+	app.init();
+});
+
+var app = { 
+	init: function() { 
+		console.log("Application Started");
+	}
+};
