7773dc5ef6a7989b475554183492583d416c818d
Michael Lesher <mike.lesher@gmail.com>
2013-09-12 10:26:51 -0400

    Added /tests directory, updated app.js to Revealing Module pattern and minor improvements
    ADDED a tests directory to the root for testing scripts
    
    UPDATED app.js to a Revealing Module pattern for consistency and
    security
    
    UPDATED app.js setting variable for standard JSON format
    
    Minor Improvements

--- build/js/app.js
+++ build/js/app.js
@@ -1,31 +1,36 @@
-// Custom JS Application Code
+// Custom JS Application Code (Revealing Module Pattern)
 
 // If using JSLint for syntax debugging, include the following two lines for looser management
-/*jslint browser: true, sloppy: true, white: true*/
-/*global $, console, alert, app*/
+/*jslint browser: true, sloppy: true, white: true, vars: true*/
+/*global $, console, alert, App*/
 
 $(function() { 
-	app.init();
+	App.init();
 });
 
-var app = { 
+var App = (function() { 
 
-	init: function() { 
+	var settings = { 
+		"name": "My Application",	
+		"url": "application_url.com",
+		"version": "1.0.0"	
+	};
+
+	var listen = function() { 
+		// Application Listeners can be loaded here for easy configuration		
+		console.log("Ready and Listening");
+	};	
+
+	var init = function() {
 		// Kick off the listeners
-		this.listeners();		
+		listen();
+		
 		// Application has been initalized
-		console.log(this.settings.name + "(v" + this.settings.version + ") Started");
-	},
+		console.log(settings.name + "(v" + settings.version + ") Started");	
+	};
+		
+	return {
+		init: init
+	};
 	
-	listeners: function() {
-		// Application Listeners can be loaded here for easy configuration
-		console.log("Listening...");
-	},
-	
-	settings: { 	
-		name: "My Application",	
-		url: "application_url.com",
-		version: "1.0.0"
-	}
-	
-};
+}());
